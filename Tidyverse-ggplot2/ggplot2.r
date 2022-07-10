e20b2036aefa310aa7dd02eb8c3ee2e29720c2cc
Michael Maxi <107141374+michaelmaxi@users.noreply.github.com>
2022-07-10 12:51:16 +1000

    Update ggplot2.r

--- Tidyverse-ggplot2/ggplot2.r
+++ Tidyverse-ggplot2/ggplot2.r
@@ -9,4 +9,9 @@ library(palmerpenguins)
 data("penguins")
 View(penguins)
 
+# Basic Plot
 ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+# Plot with colour to different species
+# ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species))
+# You can change colour, size, shape all by adding within the 'aes' brackets
