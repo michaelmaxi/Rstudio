ed255dc4f80fdace37dc3f19358024400e5ce795
Michael Maxi <107141374+michaelmaxi@users.noreply.github.com>
2022-07-10 13:43:02 +1000

    Update ggplot2.r

--- Tidyverse-ggplot2/ggplot2.r
+++ Tidyverse-ggplot2/ggplot2.r
@@ -10,13 +10,17 @@ data("penguins")
 View(penguins)
 
 # Basic Plot
-# ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
 
-# Plot with colour to different species
-# ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species))
+# Plot with colour to different species + heading
+ ggplot(data = penguins) + 
+   geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species))+
+   labs(title="TITLE - Flipper Length (mm) vs Body Mass (g)",subtitle="Data collected 1999")+
+   annotate("Text", x=220, y=4300, label="Boom", color="Purple", fontface="bold")
 # You can change colour, size, shape all by adding within the 'aes' brackets
 
 # Different Geom SMOOTH + JITTER 
 ggplot(data = penguins) + 
   geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g)) + 
-  geom_jitter(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
